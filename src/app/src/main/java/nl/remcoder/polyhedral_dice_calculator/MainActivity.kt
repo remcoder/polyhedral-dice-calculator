65d055eadcb74eec7868965ba01135a1fa5058b6
Remco Veldkamp <remcoder@gmail.com>
2018-06-21 22:53:52 +0200

    Start of the new native version

---
+++ src/app/src/main/java/nl/remcoder/polyhedral_dice_calculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package nl.remcoder.polyhedral_dice_calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
